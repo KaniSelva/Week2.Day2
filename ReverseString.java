88fd3631e3da2ad269a508d2a5bb4bf24a760dce
KaniSelva <90255979+KaniSelva@users.noreply.github.com>
2021-09-14 00:43:53 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+package week2.day2;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+	String str1 = "feeling good";
+	char[] ch = str1.toCharArray();
+	for (int i=ch.length-1;i>=0;i--) {
+		System.out.println(ch[i]);
+	}
+	 
+	}
+	
+			}
+	
+	
+
+
